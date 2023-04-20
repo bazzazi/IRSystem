26b3f34c68d49e6df26532d83845fd8b15f19602
bazzazi <mohammadalibazzazi80@gmail.com>
2023-04-21 01:07:04 +0430

    dual positional query processing has been added like inverted index or inverted /1 index

--- pos_index.py
+++ pos_index.py
@@ -3,6 +3,49 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 Stopwords = set(stopwords.words('english'))
 
+class QueryProcessing:
+    def __init__(self, query) -> None:
+        self.query=query
+        self.stack=[]
+        self.temp=""
+        self.skip=0
+
+    def compute(self):
+        index=positionaIndex()
+        if re.findall('/\d',self.query)==[]:
+            self.query=re.sub(' ', ' /1 ', self.query)
+        self.query=self.query.split()
+        word1 = index.get(self.query[0])
+        word2 = index.get(self.query[2])
+        anding = set(word1).intersection(word2)
+        self.query[1] = re.sub("/", "", self.query[1])
+        answer = []
+        skip = int(self.query[1])
+
+        for i in anding:
+            pp1 = index.get(self.query[0])[i]
+            pp2 = index.get(self.query[2])[i]
+
+            plen1 = len(pp1)
+            plen2 = len(pp2)
+
+            ii = jj = 0
+
+            while ii != plen1:
+                jj=0
+                while jj != plen2:
+                    if abs(pp1[ii]-pp2[jj])==skip:
+                        answer.append({i:[pp1[ii],pp2[jj]]})
+                    elif pp2[jj] > pp1[ii]:
+                        break 
+                    jj+=1
+                ii+=1
+        if answer:
+            return answer
+        else:
+            print('No Docs matched !')
+            return 0
+
 
 def remove_special_characters(text):
     regex = re.compile('[^a-zA-Z0-9\s]')
@@ -17,7 +60,7 @@ def preprocessing(filePath):
     words = word_tokenize(text)
     words = [word for word in words if len(words)>1]
     words = [word.lower() for word in words]
-    words = [word for word in words if word not in Stopwords]
+    # words = [word for word in words if word not in Stopwords]
     return words
 
 def positionaIndex():
@@ -38,5 +81,8 @@ def positionaIndex():
 
     return result
 
-def queryProcessing(query):
-    pass
+
+
+query=QueryProcessing('explicitly read')
+for match in query.compute():
+    print(match)
